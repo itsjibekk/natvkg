c8d9fbf20904066a64c9b7125a060bc550b8c11e
Orozmamatova <itsjibekk@gmail.com>
2023-02-25 13:09:16 +0600

    git initialization

---
+++ src/main/java/com/example/natvkg/NatvkgApplication.java
@@ -0,0 +1,13 @@
+package com.example.natvkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatvkgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatvkgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/natvkg/NatvkgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.natvkg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatvkgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
